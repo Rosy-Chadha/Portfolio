24a0bc9876fea551c6cd0f85e16210d3400268f0
Rosy Chadha <rosychadha2018@gmail.com>
2021-09-05 20:33:11 +0530

    Basic Portfolio

---
+++ src/resumeData.js
@@ -0,0 +1,79 @@
+let resumeData = {
+    "imagebaseurl":"https://Rosy-Chadha.github.io/",
+    "name": "Rosy Chadha",
+    "role": "Frontend Developer and CyberSecurity Enthusiast",
+    "linkedinId":"Your LinkedIn Id",
+    "roleDescription": "I like dabbling in various parts of frontend development and love to learn about new technologies, make new designs or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/rosy-chadha-445375196/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Rosy-Chadha",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"instagram",
+          "url":"www.instagram.com",
+          "className":"fa fa-instagram"
+        }
+      ],
+    "aboutme":"I am currently a third year student at NIIT University and pursuing my B.Tech from there. I am a self taught Front End Developer, currently diving deeper into CyberSecurity. I believe that to be successful in life, one has to be consistent and keep working towards their dream.",
+    "address":"India",
+    "website":"https://github.com/Rosy-Chadha",
+    "education":[
+      {
+        "UniversityName":"NIIT University",
+        "specialization":"Cybersecurity",
+        "MonthOfPassing":"Jul",
+        "YearOfPassing":"2023",
+      },
+    ],
+    "work":[
+      {
+        "CompanyName":"Cisco",
+        "specialization":"Cyber Security Internship",
+        "MonthOfLeaving":"Jun",
+        "YearOfLeaving":"2021",
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Farmer Application",
+        "description":"WebApp",
+        "imgurl":"images/portfolio/logo.png"
+      },
+      {
+        "name":"Shopping App",
+        "description":"Webapp",
+        "imgurl":"images/portfolio/coat-female1.jpg"
+      },
+      {
+        "name":"Bugdet App",
+        "description":"MobileApp",  
+        "imgurl":"images/portfolio/ss1.png"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"Believe in yourself!!!"
+      }
+    ]
+  }
+  
+  export default resumeData
